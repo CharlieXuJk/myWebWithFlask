f561afee1a7aa3c255b4def7e18c26ee6720a7ba
GTrepublic <hans040515@gmail.com>
2020-11-02 21:09:30 +0800

    resetup my pc, so do some backup

--- app/__init__.py
+++ app/__init__.py
@@ -1,10 +1,16 @@
 from flask import Flask
 
+from app.models.boos import db
+
+
 def create_app():
     app = Flask(__name__)
     app.config.from_object('app.config')
     app.config.from_object('app.secure')
     register_blueprint(app)
+
+    db.init_app(app)
+    db.create_all(app=app)
     return app
 
 def register_blueprint(app):
---
+++ app/secure.py
@@ -0,0 +1,1 @@
+SQLALCHEMY_DATABASE_URI = 'mysql+cymysql://root:123456@localhost:3306/fisher'    #单数据库
